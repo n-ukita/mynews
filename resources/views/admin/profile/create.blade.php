6c794a1bae3a56d647aca191e513c17a875f5920
n-ukita <dreamtodoroki@gmail.com>
2022-01-19 14:09:42 +0000

    LAravel10kadai

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="Width=device-width,initial-scale=1">
+        
+        <title>Myname</title>
+    </head>
+    <body>
+        <h1>浮田　信宏</h1>
+    </body>
+</html>
